68107212d46e149a9db4c3adcddef2cd6b58e8a2
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-10-01 14:09:25 +0200

    fake user agent

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('123')
+const puppeeter = require('puppeteer')
+
+async function run (){
+    const browser = await puppeeter.launch()
+    const page = await browser.newPage()
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperties(navigator, 'platform', {get : () => 'win32'})
+        Object.defineProperties(navigator, 'productSub', {get : () => '20100101'})
+        Object.defineProperties(navigator, 'vendor', {get : () => 'win32'})
+        Object.defineProperties(navigator, 'oscpu', {get : () => 'Windows NT 10.0; Win64; x64'})
+    })
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0')
+    await page.goto('https://www.imdb.com/title/tt1190634/')
+
+    await page.screenshot({path : 'screenshots/theboys.png'})
+
+    await browser.close()
+}
+
+run()
